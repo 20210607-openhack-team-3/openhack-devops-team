cedf91501c324e954f97c3d4b0252bc42eb93606
HaeusslerAdrian <adrian+devops@adresta.ch>
2021-06-08 11:15:46 +0200

    change healthcheck again

--- apis/userprofile/handlers/healthcheck/user.js
+++ apis/userprofile/handlers/healthcheck/user.js
@@ -23,8 +23,7 @@ module.exports = {
                 next(err);
                 return;
             }
-            data.responses = { ...data.responses, additional: true };
-            res.status(status).send(data && data.responses);
+            res.status(status).send(data && { ...data.responses, additional: true });
         });
     }
 };

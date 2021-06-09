550831db3894b557cec306f732c1edde79d7baed
Sacha Bruttin <sacha@bruttin.com>
2021-06-09 09:38:39 +0200

    Changed go api to return version (#39)
    * Changed go api to return version
    
    * fix appName
    
    * Set go 1.15 + copy setting to docker image
    
    Co-authored-by: Sacha Bruttin <sacha.bruttin@osb.group>

--- apis/trips/tripsgo/healthCheckService.go
+++ apis/trips/tripsgo/healthCheckService.go
@@ -2,14 +2,34 @@ package tripsgo
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func healthcheckGet(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("./tripsgo/settings.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	type Settings struct {
+		Version string
+	}
+
+	var obj Settings
+
+	// unmarshall it
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 
-	hc := &Healthcheck{Message: "Trip Service Healthcheck V2", Status: "Healthy"}
+	hc := &Healthcheck{Message: "Trip Service Healthcheck V2", Status: "Healthy", Version: obj.Version}
 
 	json.NewEncoder(w).Encode(hc)
 }
